594527a7bf3235fd0fb0344d1a95a752645a076f
Anna <atsigip@gmail.com>
2021-12-26 22:10:47 +0300

    correction of typos diagram

--- task dev1/Program.cs
+++ task dev1/Program.cs
@@ -2,6 +2,7 @@
 
 int [] myArray = new int [10];
 
+
 void FillArray(int[]array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -9,3 +10,37 @@ void FillArray(int[]array)
         array[i] = new Random().Next(1, 20);
     }
 }
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+FillArray(myArray);
+PrintArray(myArray);
+Console.WriteLine();
+
+
+//void EvenNum(int[] myArray)
+//{
+    int[] newArray = new int[myArray.Length];
+    int j = 0;
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+      if (myArray[i] %2 == 0 && myArray[i] != 0)
+       { newArray [j] = myArray[i];
+        j++;}
+
+
+        Console.Write($"{newArray[i]} ");
+         
+    }
+    
+
+//}
+//int[] newArray = new int[10];
+//EvenNum(newArray);
